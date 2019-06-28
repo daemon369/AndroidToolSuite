158c49a00e88b87eb1431a3231d3a36eb20a0a90
daemon369 <daemon336699@gmail.com>
2019-06-28 15:41:53 +0800

    optimize DefaultLogger

--- tool/src/main/kotlin/me/daemon/tool/log/DefaultLogger.kt
+++ tool/src/main/kotlin/me/daemon/tool/log/DefaultLogger.kt
@@ -9,42 +9,62 @@ import android.util.Log
 class DefaultLogger : ILogger {
 
     override fun v(tag: Any, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.VERBOSE) return
+
         Log.v(tag.toString(), msg.joinToString(" "))
     }
 
     override fun v(tag: Any, tr: Throwable, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.VERBOSE) return
+
         Log.v(tag.toString(), msg.joinToString(" "), tr)
     }
 
     override fun d(tag: Any, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.DEBUG) return
+
         Log.d(tag.toString(), msg.joinToString(" "))
     }
 
     override fun d(tag: Any, tr: Throwable, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.DEBUG) return
+
         Log.d(tag.toString(), msg.joinToString(" "), tr)
     }
 
     override fun i(tag: Any, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.INFO) return
+
         Log.i(tag.toString(), msg.joinToString(" "))
     }
 
     override fun i(tag: Any, tr: Throwable, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.INFO) return
+
         Log.i(tag.toString(), msg.joinToString(" "), tr)
     }
 
     override fun w(tag: Any, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.WARN) return
+
         Log.w(tag.toString(), msg.joinToString(" "))
     }
 
     override fun w(tag: Any, tr: Throwable, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.WARN) return
+
         Log.w(tag.toString(), msg.joinToString(" "), tr)
     }
 
     override fun e(tag: Any, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.ERROR) return
+
         Log.e(tag.toString(), msg.joinToString(" "))
     }
 
     override fun e(tag: Any, tr: Throwable, vararg msg: Any?) {
+        if (!openLog || logLevel > ILogger.Level.ERROR) return
+
         Log.e(tag.toString(), msg.joinToString(" "), tr)
     }
 
