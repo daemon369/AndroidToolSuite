1b2d43e46135f8a2a7cc97503924c32bae179040
daemon369 <daemon336699@gmail.com>
2019-06-28 11:26:18 +0800

    add tool module

---
+++ tool/src/androidTest/java/me/daemon/tool/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package me.daemon.tool
+
+import android.content.Context
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("me.daemon.tool.test", appContext.packageName)
+    }
+}
