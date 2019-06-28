e599eabe4cd6f96e47d09c8487c8bcafebd538e8
daemon369 <daemon336699@gmail.com>
2019-06-28 16:11:34 +0800

    only print log when openLog is true

--- tool/src/main/kotlin/me/daemon/tool/log/LogUtils.kt
+++ tool/src/main/kotlin/me/daemon/tool/log/LogUtils.kt
@@ -33,70 +33,70 @@ var filter: Filter = defaultFilter
 
 fun v(tag: Any, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.VERBOSE, tag, null, msg)) return
+    if (!openLog || !filter.filter(l, Level.VERBOSE, tag, null, msg)) return
 
     l.v(tag, *msg)
 }
 
 fun v(tag: Any, tr: Throwable, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.VERBOSE, tag, tr, msg)) return
+    if (!openLog || !filter.filter(l, Level.VERBOSE, tag, tr, msg)) return
 
     l.v(tag, tr, *msg)
 }
 
 fun d(tag: Any, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.DEBUG, tag, null, msg)) return
+    if (!openLog || !filter.filter(l, Level.DEBUG, tag, null, msg)) return
 
     l.d(tag, *msg)
 }
 
 fun d(tag: Any, tr: Throwable, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.DEBUG, tag, tr, msg)) return
+    if (!openLog || !filter.filter(l, Level.DEBUG, tag, tr, msg)) return
 
     l.d(tag, tr, *msg)
 }
 
 fun i(tag: Any, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.INFO, tag, null, msg)) return
+    if (!openLog || !filter.filter(l, Level.INFO, tag, null, msg)) return
 
     l.i(tag, *msg)
 }
 
 fun i(tag: Any, tr: Throwable, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.INFO, tag, tr, msg)) return
+    if (!openLog || !filter.filter(l, Level.INFO, tag, tr, msg)) return
 
     l.i(tag, tr, *msg)
 }
 
 fun w(tag: Any, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.WARN, tag, null, msg)) return
+    if (!openLog || !filter.filter(l, Level.WARN, tag, null, msg)) return
 
     l.w(tag, *msg)
 }
 
 fun w(tag: Any, tr: Throwable, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.WARN, tag, tr, msg)) return
+    if (!openLog || !filter.filter(l, Level.WARN, tag, tr, msg)) return
 
     l.w(tag, tr, *msg)
 }
 
 fun e(tag: Any, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.ERROR, tag, null, msg)) return
+    if (!openLog || !filter.filter(l, Level.ERROR, tag, null, msg)) return
 
     l.e(tag, *msg)
 }
 
 fun e(tag: Any, tr: Throwable, vararg msg: Any?) {
     val l = logger
-    if (!filter.filter(l, Level.ERROR, tag, tr, msg)) return
+    if (!openLog || !filter.filter(l, Level.ERROR, tag, tr, msg)) return
 
     l.e(tag, tr, *msg)
 }
