58f00d251dca5702e9704448387e65fdec576633
daemon369 <daemon336699@gmail.com>
2019-06-28 17:14:49 +0800

    fix java test

--- tool/src/androidTest/java/me/daemon/tool/log/LogTestJava.java
+++ tool/src/androidTest/java/me/daemon/tool/log/LogTestJava.java
@@ -10,58 +10,58 @@ import org.junit.Test;
 public class LogTestJava {
 
     @Before
-    private void before() {
+    public void before() {
         Log.setOpenLog(true);
         Log.setLogLevel(Level.WARN);
     }
 
     @Test
-    void v() {
+    public void v() {
         Log.v(this, "2", null, 3, 4f, new Throwable("test"));
     }
 
     @Test
-    void v1() {
+    public void v1() {
         Log.v(this, new Throwable("throwable"), "2", null, 3, 4f, new Throwable("test throwable"));
     }
 
     @Test
-    void d() {
+    public void d() {
         Log.d(this, "2", null, 3, 4f, new Throwable("test throwable"));
     }
 
     @Test
-    void d1() {
+    public void d1() {
         Log.d(this, new Throwable("throwable"), "2", null, 3, 4f, new Throwable("test throwable"));
     }
 
     @Test
-    void i() {
+    public void i() {
         Log.i(this, "2", null, 3, 4f, new Throwable("test throwable"));
     }
 
     @Test
-    void i1() {
+    public void i1() {
         Log.i(this, new Throwable("throwable"), "2", null, 3, 4f, new Throwable("test throwable"));
     }
 
     @Test
-    void w() {
+    public void w() {
         Log.w(this, "2", null, 3, 4f, new Throwable("test throwable"));
     }
 
     @Test
-    void w1() {
+    public void w1() {
         Log.w(this, new Throwable("throwable"), "2", null, 3, 4f, new Throwable("test throwable"));
     }
 
     @Test
-    void e() {
+    public void e() {
         Log.e(this, "2", null, 3, 4f, new Throwable("test throwable"));
     }
 
     @Test
-    void e1() {
+    public void e1() {
         Log.e(this, new Throwable("throwable"), "2", null, 3, 4f, new Throwable("test throwable"));
     }
 
