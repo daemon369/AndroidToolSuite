c48937d9be86169924d575c463181169c7f301c3
daemon369 <daemon336699@gmail.com>
2019-06-29 22:31:14 +0800

    add kotlin demo

--- app/src/main/java/me/daemon/tool/app/MainActivity.kt
+++ app/src/main/java/me/daemon/tool/app/MainActivity.kt
@@ -2,11 +2,24 @@ package me.daemon.tool.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import me.daemon.tool.log.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logLevel = Level.VERBOSE
+        v(this, "onCreate: $savedInstanceState")
+        v(this, Throwable("onCreate: $savedInstanceState"))
+        d(this, "onCreate: $savedInstanceState")
+        d(this, Throwable("onCreate: $savedInstanceState"))
+        i(this, "onCreate: $savedInstanceState")
+        i(this, Throwable("onCreate: $savedInstanceState"))
+        w(this, "onCreate: $savedInstanceState")
+        w(this, Throwable("onCreate: $savedInstanceState"))
+        e(this, "onCreate: $savedInstanceState")
+        e(this, Throwable("onCreate: $savedInstanceState"))
     }
 }
